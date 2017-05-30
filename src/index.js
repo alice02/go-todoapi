fbd3bd5365b8dd1b6f4773e22f7b94fe4126acfa
alice02 <scene.4913@gmail.com>
2017-05-31 01:01:18 +0900

    Add react tap event plugin

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import injectTapEventPlugin from "react-tap-event-plugin";
 import App from "./containers/App";
 import rootReducer from "./reducers";
 import { getTodosIfNeeded } from "./actions";
@@ -11,6 +12,7 @@ const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(rootReducer)
 
 store.dispatch(getTodosIfNeeded())
+injectTapEventPlugin();
 
 render(
   <Provider store={store}>
