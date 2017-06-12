775e7dcf8e5d6d372afdd28838800a86a3a52792
alice02 <scene.4913@gmail.com>
2017-06-12 23:37:18 +0900

    Add store module

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import injectTapEventPlugin from "react-tap-event-plugin";
 import App from "./containers/App";
-import rootReducer from "./reducers";
+import configureStore from "./store/configureStore";
 import { getTodosIfNeeded } from "./actions";
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const store = createStoreWithMiddleware(rootReducer)
-
+const store = configureStore()
 store.dispatch(getTodosIfNeeded())
 injectTapEventPlugin();
 
